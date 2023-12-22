1dcf0906623a8713229f665fd12d44cf49d732b4
sawyer bristol <sawyerbristol@gmail.com>
2023-12-21 23:42:53 -0700

    allowing different sites to manage its own strings

--- internal/scrapper/scrapper.go
+++ internal/scrapper/scrapper.go
@@ -2,41 +2,61 @@ package scrapper
 
 import (
 	"fmt"
+	"sync"
+
 	"github.com/gocolly/colly/v2"
 )
 
-func Main() {
-	news_sources := []string{
-		"www.npr.org",
-		"www.cnbc.com",
-	}
-	scrape(news_sources)
+type Site string
 
+const (
+	CNBC Site = "www.cnbc.com"
+	NPR       = "www.npr.org"
+)
+
+func Main() {
+	news_sources := []Site{NPR} // ,CNBC
+	scrape(news_sources)
 }
 
-func scrape(domains []string) {
+func scrape(domains []Site) {
+	var wg sync.WaitGroup
+
 	for _, element := range domains {
-		scraper(element)
+		wg.Add(1)
+		scraper(&wg, element)
 	}
+	// Wait for all sites to complete search
+	wg.Wait()
 
 	fmt.Println("Done")
 }
 
-func scraper(domain string) {
+func scraper(wg *sync.WaitGroup, domain Site) {
+	defer wg.Done()
 	c := colly.NewCollector(
-		colly.AllowedDomains(domain),
+		colly.AllowedDomains(string(domain)),
 	)
 
 	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		e.Request.Visit(e.Attr("href"))
-		fmt.Println(e)
 	})
 
 	//https://oxylabs.io/blog/golang-web-scraper
+	switch domain {
+	case CNBC:
+		c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+			fmt.Println(e.Text)
+		})
+	case NPR:
+		c.OnHTML("p", func(e *colly.HTMLElement) {
+			fmt.Println(e.Text)
+		})
+	}
 
-	website := "https://" + domain
+	website := "https://" + string(domain)
 	c.Visit(website)
-	fmt.Println("Finished ", domain)
 
+	fmt.Println("Finished ", domain)
 }
